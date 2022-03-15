75f20ddb639279b3bc21698b29dff851e4b08801
lwightwick <wightwick.lucy21@gmail.com>
2022-03-14 21:35:00 -0700

    fonts, colours, breakpoints set for styling reset file added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <h2>oh heyy h2</h2>
+      <h3>im h3</h3>
+      <p>im the paragraph!!</p>
+      <p2>im a p2</p2>
+      <p3>guess im a p3</p3>
     </div>
   );
 }
