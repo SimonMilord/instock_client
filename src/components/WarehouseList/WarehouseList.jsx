f1ba9d2332ceccbd5dc91d6c2857b528415f5290
Simon Milord <simon.milord@gmail.com>
2022-03-15 20:39:34 -0400

    added structure and styling for search bar

---
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WarehouseList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>there goes the list</h2>
+      </div>
+    );
+  }
+}
+
+export default WarehouseList;
--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import WarehouseList from '../../components/WarehouseList/WarehouseList';
 import "./WarehousesPage.scss";
+import searchIcon from "../../assets/Icons/search-24px.svg";
 
 class WarehousesPage extends Component {
   render() {
     return (
-      <div>
-        <h1>WAREHOUSES</h1>
+      <div className='warehousePage'>
+        <div className='warehousePage-subhead'>
+          <h1 className='warehousePage-title'>Warehouses</h1>
+          <div className='warehousePage-subhead__right'>
+            <div className='search'>
+              <input className="search__input" type="text" name="" placeholder='Search...' />
+              <img className='search__icon' src={searchIcon} alt="search icon"></img>
+            </div>
+            <button className='newWarehouse-btn'>+ Add New Warehouse</button>
+          </div>
+        </div>
+        <WarehouseList />
       </div>
     );
   }
