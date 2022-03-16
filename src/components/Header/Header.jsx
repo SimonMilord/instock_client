5fa14195fa2edaf97f316695cb043c772438b928
carosteguig <carosteguig@gmail.com>
2022-03-15 21:35:40 -0700

    Added styling to mobile, tablet and desktop header's views. Buttons' details remaining

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,23 @@
 import './Header.scss';
 import { Link } from 'react-router-dom';
-import InStockLogo from '../../assets/Logo/InStock-Logo_1x.png';
+import InStockLogo from '../../assets/Logo/InStock-Logo_2x.png';
 
 
 function Header() {
     return (
         <nav className='nav'>
-            <Link className='nav__logo-link' to='/'>
-                <img className='nav__logo-img' src={InStockLogo} alt='InStock logo' />
-            </Link>
-            <div className='nav__list'>
-                <Link className='nav__list-link' to='/'>
-                    WareHouses
+            <div className='nav__container'>
+                <Link className='nav__logo-link' to='/'>
+                    <img className='nav__logo-img' src={InStockLogo} alt='InStock logo' />
                 </Link>
-                <Link className='nav__list-link' to='/Inventory'>
-                    Inventory
-                </Link>
+                <div className='nav__list'>
+                    <Link className='nav__list-link--warehouses' to='/'>
+                        <h3 className='nav__list-text--warehouses'>Warehouses</h3>
+                    </Link>
+                    <Link className='nav__list-link--inventory' to='/Inventory'>
+                        <h3 className='nav__list-text--inventory'>Inventory</h3>
+                    </Link>
+                </div>
             </div>
         </nav>
 
