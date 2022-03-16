303f51974d50a092cfc3f694e15ad4fd69d064c3
Simon Milord <simon.milord@gmail.com>
2022-03-16 09:50:47 -0400

    added styling for warehouse list item on mobile

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import "./WarehouseList.scss";
 import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import trashCan from "../../assets/Icons/delete_outline-24px.svg";
@@ -14,28 +14,30 @@ class WarehouseList extends Component {
             <div className='whLi__subbox whLi__subbox--left'>
               <div className='whLi__item whLi__item--link'>
                 <h4 className='whLi__label'>Warehouse</h4>
-                <Link className='whLi__link' path="/">Manhattan</Link>
-                <img className='whLi__link-icon' src={chevron} alt="chevron"></img>
+                <div className='whLi__link-box'>
+                  <p2 className='whLi__link'>Manhattan</p2>
+                  <img className='whLi__link-icon' src={chevron} alt="chevron"></img>
+                </div>
               </div>
               <div className='whLi__item'>
                 <h4 className='whLi__label'>Address</h4>
-                <p className='whLi__info'>503 Broadway, New York, USA</p>
+                <p2 className='whLi__info'>503 Broadway, New York, USA</p2>
               </div>
             </div>
             <div className='whLi__subbox whLi__subbox--right'>
               <div className='whLi__item'>
                 <h4 className='whLi__label'>Contact Name</h4>
-                <p className='whLi__info'>Parmin Aujla</p>
+                <p2 className='whLi__info'>Parmin Aujla</p2>
               </div>
               <div className='whLi__item'>
                 <h4 className='whLi__label'>Contact Information</h4>
-                <p className='whLi__info'>+1 (629) 555-0129 paujla@instock.com</p>
+                <p2 className='whLi__info'>+1 (629) 555-0129 paujla@instock.com</p2>
               </div>
             </div>
           </div>
           <div className='whLi__actions'>
-            <img className="whLi__deleteBtn" src={trashCan} alt="trash can icon"></img>
-            <img className="whLi__editBtn" src={editPen} alt="edit pen icon"></img>
+            <img className="whLi__deleteBtn iconBtn" src={trashCan} alt="trash can icon"></img>
+            <img className="whLi__editBtn iconBtn" src={editPen} alt="edit pen icon"></img>
           </div>
         </div>
       </div>
