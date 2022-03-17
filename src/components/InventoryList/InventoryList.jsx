e74d710de06c17eb5d65a0a4bc8508d277e10862
carosteguig <carosteguig@gmail.com>
2022-03-17 14:23:32 -0700

    working in inv list stylea and axios get request

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -2,22 +2,40 @@ import './InventoryList.scss';
 import React from 'react';
 import Delete from '../../assets/Icons/delete_outline-24px.svg';
 import Edit from '../../assets/Icons/edit-24px.svg';
+import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import { Link } from 'react-router-dom';
 // import axios from 'axios';
 
 
+
 class InventoryList extends React.Component {
 
-    state = {
-        inventoryData: [],
+    // state = {
+    //     inventories: [],
+    // };
 
+    // componentDidMount() {
+    //     this.getAllInventories();
+    // }
 
+    // getAllInventories() {
+    //     axios
+    //         .get(`${process.env.REACT_APP_API_URL}/inventory`)
+    //         .then((res) => {
+    //             console.log(res);
+    //             this.setState({
+    //                 inventories: res.data
+    //             });
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    // }
 
-    }
 
     render() {
 
-
+        // const { inventories } = this.state;
 
         return (
 
@@ -43,18 +61,21 @@ class InventoryList extends React.Component {
                             <li className='inventory__list-item inventory__icon-6'>actions</li>
                         </ul>
                     </header>
+                   
                     <body className='card'>
                         <div className='card__section'>
                             <section className='card__content'>
                                 <div className='card__content-div'>
                                     <h4 className='card__titles'>inventory item</h4>
                                     <Link className='card__text-link'>
-                                        <h3 className='card__text-btn'>Phone Charger</h3>
+                                        {/* <h3 className='card__text-btn'>{inventories.itemName}</h3> */}
+                                        <h3 className='card__text-btn'>Cell Phone<img className='card__text-icon' src={chevron} alt="chevron"/></h3>
                                     </Link>
                                 </div>
                                 <div className='card__content-div'>
                                     <h4 className='card__titles card__titles--padding'>category</h4>
-                                    <p className='card__text'>Electronics</p>
+                                    {/* <p className='card__text'>{inventories.category}</p> */}
+                                    <p className='card__text'>electronics</p>
                                 </div>
                             </section >
                             <section className='card__content'>
@@ -64,28 +85,31 @@ class InventoryList extends React.Component {
                                         <h3 className='card__text--in-stock active'>in stock</h3>
                                     </div>
                                     <div className='card__out-stock'>
+                                        {/* <h3 className='card__text--in-stock active'>{inventories.status}</h3> */}
                                         <h3 className='card__text--in-stock active'>in stock</h3>
                                     </div>
                                 </div>
                                 <div className='card__content-div'>
                                     <h4 className='card__titles'>qty</h4>
+                                    {/* <p className='card__text'>{inventories.quantity}</p> */}
                                     <p className='card__text'>500</p>
                                 </div>
                                 <div className='card__content-div'>
                                     <h4 className='card__titles'>warehouse</h4>
-                                    <p className='card__text'>King West</p>
+                                    {/* <p className='card__text'>{inventories.warehouseName}</p> */}
+                                    <p className='card__text'>New York</p>
                                 </div>
                             </section>
                         </div>
                         <section className='card__content-bottom'>
-                            <Link to='/'>
+                            <Link to=''>
                                 <img className='card__btn-delete' src={Delete} alt='delete Icon' />
                             </Link>
-                            <Link to='/'>
+                            <Link to=''>
                                 <img className='card__btn-edit' src={Edit} alt='Edit Icon' />
                             </Link>
                         </section>
-                    </body>
+                    </body> 
 
                 </section>
             </div>
