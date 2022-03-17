71a0f0bb62a5b4038a9916cd758eddd14d215fd3
Brodie <brodiesnelling@gmail.com>
2022-03-17 19:14:48 -0400

    started modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+
+export default class Modal extends Component {
+    render () {
+        return (
+            <div className="modal">
+                hello there
+            </div>
+        )
+    }
+}
--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -3,18 +3,19 @@ import axios from 'axios';
 import './WarehouseDetails.scss';
 import { Link } from 'react-router-dom';
 import WarehouseInv from '../WarehouseInv/WarehouseInv';
-
+import Modal from '../Modal/Modal';
 
 export default class WarehouseDetails extends Component {
 
     state = {
         warehouseData: {},
         inventory: [],
+        popUp: false
     }
 
     async fetchInventory (id) {
         try {
-            const invResponse = await axios.get(`${process.env.REACT_APP_API_URL}/warehouse/${id}/inventory`)
+            const invResponse = await axios.get(`${process.env.REACT_APP_API_URL}/warehouses/${id}/inventory`)
             this.setState ({
                 inventory: invResponse.data
             })
@@ -23,7 +24,7 @@ export default class WarehouseDetails extends Component {
 
     async fetchWarehouseData (id) {
         try {
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/warehouse/${id}`)
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/warehouses/${id}`)
             this.setState({
                 warehouseData: response.data,
             })
@@ -31,12 +32,20 @@ export default class WarehouseDetails extends Component {
         } catch(err) {console.log(err)}
     }
 
-    componentDidMount (){
+    componentDidMount () {
         this.fetchWarehouseData(this.props.match.params.id);
+    }
+
+
+    handlePopUp() {
+        this.setState ({
+            popUp: !this.state.popUp
+        }) 
     }
 
     render() {
         return (
+            <>
             <div className='details'>
                 <div className='details__titlebar'>
                     <Link className='details__link'>
@@ -77,6 +86,8 @@ export default class WarehouseDetails extends Component {
                     <WarehouseInv inventory = {this.state.inventory} />
                 </div>
             </div>
+            {this.state.popUp === true ? (<Modal warhouseInv={this.state.inventory}/>) : (console.log("no modal"))}
+            </>
         )
     }
 }
