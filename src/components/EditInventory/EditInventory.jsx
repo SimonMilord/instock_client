551f7503ca9a1179fd3002fcd7edc60c098a2752
lwightwick <wightwick.lucy21@gmail.com>
2022-03-18 14:52:39 -0700

    form fields populating
    Co-authored-by: bsnelling9 <bsnelling9@users.noreply.github.com>

--- src/components/EditInventory/EditInventory.jsx
+++ src/components/EditInventory/EditInventory.jsx
@@ -5,237 +5,162 @@ import arrowBack from "../../assets/Icons/arrow_back-24px.svg"
 import axios from "axios";
 
 
-// getInventoryData(){
-//     axios
-//     .get(`${process.env.REACT_APP_API_URL}/inventory`)
-//     .then((response) =>{
-//         this.setState({
-//             inventory: response.data,
-//         });
-        
-//     })
-// }
-
-// getWarehouseData(){
-//     axios
-//     .get(`${process.env.REACT_APP_API_URL}/warehouse`)
-//     .then((response) =>{
-//         this.setState({
-//             warehouse: response.data,
-//         });
-// })
-// };
-
-// // componentDidMount(){
-    
-   
-// // }
-
-    // handleFormSubmit = (e) => {
-    //     e.preventDefault();
-    //     updateInventory(id, updateInventory)
-    // }
-// // function EditInventory(props) {
-
-// function EditInventory(props)
-
     class EditInventory extends Component {
         state ={
-            inventory:{ "id": "83433026-ca32-4c6d-bd86-a39ee8b7303e",
-            "warehouseID": "2922c286-16cd-4d43-ab98-c79f698aeab0",
-            "warehouseName": "Manhattan",
-            "itemName": "Gym Bag",
-            "description": "Made out of military-grade synthetic materials, this gym bag is highly durable, water resistant, and easy to clean.",
-            "category": "Gear",
-            "status": "Out of Stock",
-            "quantity": 0},}
-        
+            inventory: [],
+            warehouseData: [],
+            name: "",
+            description: "",
+            category: "",
+            status: "",
+            quantity: 0,
+            warehouse: "",
+        }
 
-        // handleFormSubmit(event){
-        //     const itemName = event.target.itemName.value;
-        //     const description = event.target.description.value;
-        //     const category = event.target.category.value;
-        //     const status = event.target.status.value;
-        //     const warehouse = event.target.warehouse.value;
-        // }
-            render(){
-                return(
-                    <div className="editInventory">
-                             <div className="editInventoryHeader">
-                    <img className="editInventoryHeader__icon" src={arrowBack} alt="arrow back"></img>
-                    <h1 className="editInventoryHeader__heading">edit inventory item</h1>
-                </div>
-                <div className="editInventoryForm">
-                <h2 className="editInventoryForm__heading">Item Details</h2>
-                        <form className="editInventoryForm__form" action="">
-                        {/* onSubmit={handleFormSubmit} */}
-                            <div className="editInventoryForm__field">
+        getMappedWarehouses = (warehouseData) => {
+            const mapWarehouse = warehouseData.map((warehouse, index) => {
+                return (
+                    <option key={warehouse.id} className="editInventoryForm__select--option" value={index}>{warehouse.name}</option>
+                )
+            })
+            return mapWarehouse
+        }
+
+        async fetchWarhouseData () {
+            const allWarehouses = await axios.get(`${process.env.REACT_APP_API_URL}/warehouses`)
+            this.setState({
+                warehouseData: allWarehouses.data
+            })
+        }
+
+        componentDidMount(){
+            axios.get(`${process.env.REACT_APP_API_URL}/inventory/${this.props.match.params.id}`)   
+            .then(res => {
+                this.setState({name:res.data.itemName, description:res.data.description, category:res.data.category, status:res.data.status, quantity:res.data.quantity, warehouse:res.data.warehouse});    
+            })
+            this.fetchWarhouseData()                  
+        }
+    
+        editName = event =>{
+            this.setState({name: event.target.value});
+        }
+    
+        editDescription = event => {
+            this.setState({description: event.target.value});
+        }
+
+        editStatus = event => {
+            this.setState({status: event.target.value});
+        }
+
+        editQuantity = event => {
+            this.setState({quantity: event.target.value});
+        }
+       
+        handleSubmit() {
+            const itemName = this.state.name;
+            const description = this.state.description;
+            const category = this.state.category;
+            const status = this.state.status;
+            const quantity = this.state.quantity;
+            const warehouse = this.state.warehouse;
+            
+        }
+
+    render(){
+        return(
+            <div className="editInventory">
+                
+                     <div className="editInventoryHeader">
+            <img className="editInventoryHeader__icon" src={arrowBack} alt="arrow back"></img>
+            <h1 className="editInventoryHeader__heading">edit inventory item</h1>
+        </div>
+        <div className="editInventoryForm">
+            
+            <form className="editInventoryForm__form" action="" onSubmit={this.handleSubmit}>
+                <div className="editInventoryForm__fields-container">
+                        <h2 className="editInventoryForm__heading">Item Details</h2>
+                    {/* onSubmit={handleFormSubmit} */}
+                        <div className="editInventoryForm__field">
                             <label className="editInventoryForm__label"><h3>Item Name</h3></label>
                             <input
-                            className="editInventoryForm__input"
-                type="text"
-                placeholder="itemName"
-                name="Item Name"
-                value={this.state.inventory.itemName}
-                // onChange={(event)=> itemName(event.target.value)}
-                required
-                ></input> 
-                  </div>
-                  <div className="editInventoryForm__field">
-                       <label className="editInventoryForm__label"><h3>Description</h3></label>
+                                className="editInventoryForm__input"
+                                type="text"
+                                // placeholder={this.state.name.value}
+                                name="Item Name"
+                                value={this.state.name}
+                                onInput={this.editName}
+                                required
+                            ></input> 
+                        </div>
+                        <div className="editInventoryForm__field">
+                            <label className="editInventoryForm__label"><h3>Description</h3></label>
                             <textarea className="editInventoryForm__textarea"
-                type="text"
-                placeholder="description"
-                name="Description"
-                value={this.state.inventory.description}
-                // onChange={(e)=> setName(e.target.value)}
-                required
-                ></textarea> 
-</div>
-<div className="editInventoryForm__field">
-<label className="editInventoryForm__label"><h3>Category</h3></label>
-<select className="editInventoryForm__input" name="Category" id="" placeholder="Please select">
-<option className="editInventoryForm__select--option" value="1">Electronics</option>
-                                    <option className="editInventoryForm__select--option" value="2">Gear</option>
-                                    <option className="editInventoryForm__select--option" value="3">Apparel</option>
-                                    <option className="editInventoryForm__select--option" value="4">Accessories</option>
-                                    <option className="editInventoryForm__select--option" value="5">Health</option>
-    
-</select>
-
-</div>
-<h2 className="editInventoryForm__heading">Item Availability</h2>
-<div className="editInventoryForm__field">
-    <h3>Status</h3>
+                                type="text"
+                                placeholder="description"
+                                name="Description"
+                                value={this.state.description}
+                                onInput={this.editDescription}
+                                required
+                            ></textarea> 
+                        </div>
+                        <div className="editInventoryForm__field">
+                            <label className="editInventoryForm__label"><h3>Category</h3></label>
+                            <select className="editInventoryForm__input" name="Category" id="" placeholder="Please select">
+                                <option className="editInventoryForm__select--option" value="1">Electronics</option>
+                                <option className="editInventoryForm__select--option" value="2">Gear</option>
+                                <option className="editInventoryForm__select--option" value="3">Apparel</option>
+                                <option className="editInventoryForm__select--option" value="4">Accessories</option>
+                                <option className="editInventoryForm__select--option" value="5">Health</option>
+                            </select>
+                        </div>
+                        <h2 className="editInventoryForm__heading">Item Availability</h2>
+                        <div className="editInventoryForm__field">
+                            <h3>Status</h3>
                             <div className="editInventoryForm__status">
-                                <input className="editInventoryForm__status--radio" type="radio" id="" name="stock_status" value="HTML"/>
+                                <input className="editInventoryForm__status--radio" type="radio" id="" name="stock_status" value='In Stock' checked={this.state.status === "In Stock"} onChange={this.editStatus}/>
                                 <label className="editInventoryForm__labels">In Stock</label>
-                                <input className="editInventoryForm__status--radio" type="radio" id="" name="stock_status" value="CSS"/>
+                                <input className="editInventoryForm__status--radio" type="radio" id="" name="stock_status" value='Out of Stock' checked={this.state.status === "Out of Stock"} onChange={this.editStatus}/>
                                 <label className="editInventoryForm__labels">Out Of Stock</label>
                             </div>
-</div>
-<div className="editInventoryForm__field">
-<label className="editInventoryForm__label"><h3>Warehouse</h3></label>
-<select className="editInventoryForm__input">
-<option className="editInventoryForm__select--option" value="1">Manhattan</option>
-                                <option className="editInventoryForm__select--option" value="2">Washington</option>
-                                <option className="editInventoryForm__select--option" value="3">Jersey</option>
-                                <option className="editInventoryForm__select--option" value="4">San Fran</option>
-                                <option className="editInventoryForm__select--option" value="5">Santa Monica</option>
-                                <option className="editInventoryForm__select--option" value="5">Seattle</option>
-                                <option className="editInventoryForm__select--option" value="5">Miami</option>
-</select>
-</div>
+                        </div>
+                        <div className="editInventoryForm__field" style={{display: this.state.status === 'In Stock' ? "flex": "none", flexDirection: "column"}}>
+                            <label className="editInventoryForm__label"><h3>Quantity</h3></label>
+                            <input
+                                className="editInventoryForm__input"
+                                type="text"
+                                name="Item Name"
+                                value={this.state.quantity}
+                                onInput={this.editQuantity}
+                                required
+                            ></input> 
+                        </div>
 
-<button type="button" onClick={(event) => (window.location.href = "/inventory")}>
-            Cancel
-         </button>
-         <button type="submit" >
-            Save
-       </button>
-                          
-        
-        
-                        </form>
-                    </div></div>
-                )
-            }
-        }
-        
+                        <div className="editInventoryForm__field">
+                            <label className="editInventoryForm__label"><h3>Warehouse</h3></label>
+                            <select className="editInventoryForm__input">
+                                {this.getMappedWarehouses(this.state.warehouseData)}
+                            </select>
+                        </div>
+                </div>
+                <div className="button__container">
+                <button className="button" type="button" onClick={(event) => (window.location.href = "/inventory")}>
+                    Cancel
+                    </button>
+                    <button className="button" type="submit" >
+                    Save
+                </button>
+                </div>                     
 
 
-// render(){    
-// return (
-//         <div>
-//                         <div className="header">
-//                     <img className="header__icon" src={arrowBack} alt="arrow back"></img>
-//                     <h1 className="header__heading"> Edit Inventory</h1>
-//                 </div>
-//         <form onSubmit={handleFormSubmit}>
-//         <div>
-//             <label>Item Name</label>
-//                 <input
-//                 type="text"
-//                 placeholder="itemName *"
-//                 name="Item Name"
-//                 value={itemName}
-//                 onChange={(e)=> setName(e.target.value)}
-//                 required
-//                 ></input> 
-//                 </div>
-//         <div>
-//             <label>Description</label>
-//             <textarea
-//                 type="textarea"
-//                 placeholder="description *"
-//                 name="Description"
-//                 value={description}
-//                 onChange={(e)=> setDescription(e.target.value)}
-//                 required
-//                 >{"This is where the text goes"}</textarea>
-//         </div>
-//         <div>
-//         <label className="itemDetails__labels">Category</label>
-//                                 <Select className="itemDetails__select" name="category" id="" placeholder="Please select">
-//                                     <option className="itemDetails__select--option" value="1">Electronics</option>
-//                                     <option className="itemDetails__select--option" value="2">Gear</option>
-//                                     <option className="itemDetails__select--option" value="3">Apparel</option>
-//                                     <option className="itemDetails__select--option" value="4">Accessories</option>
-//                                     <option className="itemDetails__select--option" value="5">Health</option>
-//                                 </Select>
-//        {/* <label>Category</label>
-//        <textarea
-//                 type="select"
-//                 // placeholder="itemName *"
-//                 name="Category"
-//                 value={category}
-//                 onChange={(e)=> setCategory(e.target.value)}
-//                 required
-//                 ></textarea> */}
-//         </div>
-//         <div>
-//        <label>Status</label>
-//        <input
-//                 // type="select"
-//                 // placeholder="itemName *"
-//                 name="Status"
-//                 value={status}
-//                 onChange={(e)=> setStatus(e.target.value)}
-//                 required
-//                 ></input>
-//         </div>
-//         <div>
-//         <Select className="itemDetails__select" name="Please select" id="">
-//                                 <option className="itemDetails__select--option" value="1">Manhattan</option>
-//                                 <option className="itemDetails__select--option" value="2">Washington</option>
-//                                 <option className="itemDetails__select--option" value="3">Jersey</option>
-//                                 <option className="itemDetails__select--option" value="4">San Fran</option>
-//                                 <option className="itemDetails__select--option" value="5">Santa Monica</option>
-//                                 <option className="itemDetails__select--option" value="5">Seattle</option>
-//                                 <option className="itemDetails__select--option" value="5">Miami</option>
-//                             </Select>
-//        {/* <label>Warehouse</label>
-//        <input
-//                 type="select"
-//                 // placeholder="itemName *"
-//                 name="Warehouse"
-//                 value={warehouse}
-//                 onChange={(e)=> setWarehouse(e.target.value)}
-//                 required
-//                 ></input> */}
-//         </div>
-//         <button type="button" onClick={(event) => (window.location.href = "/inventory")}>
-//            Cancel
-//         </button>
-//         <button type="submit" >
-//            Save
-//         </button>
-//     </form>
-//     </div>
+                    </form>
+                </div></div>
+        )
+    }
+}
 
-//     )
-// }
-// }
+
+
 
 
 
