24da518187591fccc4d3e74c8523720a8610705a
carosteguig <carosteguig@gmail.com>
2022-03-18 03:43:54 -0700

    Inventory card and list almost ready. Need to sort some details. Out of stock and margin

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -1,60 +1,61 @@
+import './InventoryCard.scss';
 import Delete from '../../assets/Icons/delete_outline-24px.svg';
 import Edit from '../../assets/Icons/edit-24px.svg';
-// import chevron from "../../assets/Icons/chevron_right-24px.svg";
+import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import { Link } from 'react-router-dom';
 
 function InventoryCard(props) {
 
     return (
 
-        <body className='card'>
-        <div className='card__section'>
-            <section className='card__content'>
-                <div className='card__content-div'>
-                    <h4 className='card__titles'>inventory item</h4>
-                    <Link className='card__text-link'>
-                        <h3 className='card__text-btn'>{props.itemName}</h3>
-                        {/* <h3 className='card__text-btn'>Cell Phone<img className='card__text-icon' src={chevron} alt="chevron"/></h3> */}
+        <body className='cardList'>
+            <div className='card '>
+                <div className='card__box'>
+                    <section className='card__subbox card__subbox--left'>
+                        <div className='card__item card__item--link'>
+                            <h4 className='card__label'>inventory item</h4>
+                            <div className='card__link-box'>
+                                <Link className='card__link-anchor' to='/inventory/:id'>
+                                    <h3 className='card__link'>{props.itemName}</h3>
+                                </Link>
+                                <img className='card__link-icon' src={chevron} alt="chevron"></img>
+                            </div>
+                        </div>
+                        <div className='card__item'>
+                            <h4 className='card__label'>category</h4>
+                            <p className='card__info card__info--category'>{props.category}</p>
+                        </div>
+                    </section >
+                    <section className='card__subbox card__subbox--right'>
+                        <div className='card__item'>
+                            <h4 className='card__label'>status</h4>
+                            <div className='card__stock--green active'>
+                                <h3 className='card__stock--in'>{props.status}</h3>
+                            </div>
+                            <div className='card__stock--red active'>
+                                <h3 className='card__stock--out'>{props.status}</h3>
+                            </div>
+                        </div>
+                        <div className='card__item'>
+                            <h4 className='card__label'>qty</h4>
+                            <p className='card__info card__info--qty'>{props.quantity}</p>
+                        </div>
+                        <div className='card__item'>
+                            <h4 className='card__label'>warehouse</h4>
+                            <p className='card__info card__info--warehouse'>{props.warehouseName}</p>
+                        </div>
+                    </section>
+                </div>
+                <section className='card__actions'>
+                    <Link to=''>
+                        <img className='card__deleteBtn iconBtn' src={Delete} alt='delete Icon' />
                     </Link>
-                </div>
-                <div className='card__content-div'>
-                    <h4 className='card__titles card__titles--padding'>category</h4>
-                    <p className='card__text'>{props.category}</p>
-                    {/* <p className='card__text'>electronics</p> */}
-                </div>
-            </section >
-            <section className='card__content'>
-                <div className='card__content-div'>
-                    <h4 className='card__titles'>status</h4>
-                    <div className='card__in-stock'>
-                        <h3 className='card__text--in-stock active'>in stock</h3>
-                    </div>
-                    <div className='card__out-stock'>
-                        <h3 className='card__text--in-stock active'>{props.status}</h3>
-                        {/* <h3 className='card__text--in-stock active'>in stock</h3> */}
-                    </div>
-                </div>
-                <div className='card__content-div'>
-                    <h4 className='card__titles'>qty</h4>
-                    <p className='card__text'>{props.quantity}</p>
-                    {/* <p className='card__text'>500</p> */}
-                </div>
-                <div className='card__content-div'>
-                    <h4 className='card__titles'>warehouse</h4>
-                    <p className='card__text'>{props.warehouseName}</p>
-                    {/* <p className='card__text'>New York</p> */}
-                </div>
-            </section>
-        </div>
-        <section className='card__content-bottom'>
-            <Link to=''>
-                <img className='card__btn-delete' src={Delete} alt='delete Icon' />
-            </Link>
-            <Link to=''>
-                <img className='card__btn-edit' src={Edit} alt='Edit Icon' />
-            </Link>
-        </section>
-    </body> 
+                    <Link to=''>
+                        <img className='card__editBtn iconBtn' src={Edit} alt='Edit Icon' />
+                    </Link>
+                </section>
+            </div>
+        </body>
 
     )
 }
